39334acbaefe866254423cc4939cef723625473d
jinhyeon <kjh_5771@naver.com>
2019-08-01 13:02:40 +0900

    blog.models.Post의 photo 필드에 upload_to 인자 지정

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.') #길이 제한 있는 문자열
     content = models.TextField(verbose_name='내용') #길이 제한 없는 문자열
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d') #업로드 파일을 디렉토리별 시간대별로 관리함
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True, help_text='경도/위도 포맷으로 입력',
         validators=[lnglat_validator],
