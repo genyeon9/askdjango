3319a33494f88d0a85c504581f2ecc63d888c240
jinhyeon <kjh_5771@naver.com>
2019-08-04 21:54:02 +0900

    퀴즈를 맞처야 로그인

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,26 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from .models import Profile
 
 class SignupForm(UserCreationForm):
+    phone_number = forms.CharField()
+    address = forms.CharField()
+
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ['email']
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+    def save(self):
+        user = super().save()
+        profile = Profile.objects.create(
+            user=user,
+            phone_number=self.cleaned_data['phone_number'],
+            address=self.cleaned_data['address'])
+        return user
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None) #answer값이 있으면 가져오고 없으면 None을 리턴
+        if answer != 6:
+            raise forms.ValidationError('mismatched!')
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
+from .forms import LoginForm
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html',
+                                     authentication_form=LoginForm), name='login'),
     path('logout/', LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
     path('profile/', views.profile, name='profile'),
 
