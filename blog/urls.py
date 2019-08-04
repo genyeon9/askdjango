bf6c2be4972932994db1c710a5890ec8ede09051
jinhyeon <kjh_5771@naver.com>
2019-08-04 10:51:08 +0900

    CBV : CreateView/UpdateView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, re_path
 from . import views, views_cbv
 
-app_name = 'blog' #프로젝트 레벨에서 namesapce 지정 후 app_name 지정 필요함
+app_name = 'blog'  # 프로젝트 레벨에서 namesapce 지정 후 app_name 지정 필요함
 urlpatterns = [
-    path('', views_cbv.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('new/', views.post_new, name='post_new'),
     path('<int:id>/edit/', views.post_edit, name='post_edit'),
 
+    path('cbv/', views_cbv.post_list),
     path('cbv/new/', views_cbv.post_new),
+    path('cbv/<int:pk>/edit/', views_cbv.post_edit),
 ]
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,16 +1,10 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from . models import Post
-from django import forms
 
 post_list = ListView.as_view(model=Post, paginate_by=10)
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
+post_detail = DetailView.as_view(model=Post)
 
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
+post_new = CreateView.as_view(model=Post, fields='__all__')
 
-post_new = PostCreateView.as_view()
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
