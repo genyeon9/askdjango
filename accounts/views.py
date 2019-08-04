cbb2bd2c601da6f1c2042e08cf1d02c32250e1d7
jinhyeon <kjh_5771@naver.com>
2019-08-04 21:14:09 +0900

    회원가입시 이메일 추가 입력

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
 # Create your views here.
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)  # default값은 "/accounts/login/"
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
 
-@login_required
+@login_required  #로그인 시에만 프로필 템플릿이 뜨도록 하는 방법
 def profile(request):
     return render(request, 'accounts/profile.html')
