52cb05c473cb5b97ea538052c4d83db261fcbbf7
jinhyeon <kjh_5771@naver.com>
2019-08-04 10:26:04 +0900

    CBV : ListView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views, views_cbv
 
 app_name = 'blog' #프로젝트 레벨에서 namesapce 지정 후 app_name 지정 필요함
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views_cbv.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('new/', views.post_new, name='post_new'),
     path('<int:id>/edit/', views.post_edit, name='post_edit'),
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,6 +1,8 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from . models import Post
 from django import forms
+
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
 class PostForm(forms.ModelForm):
     class Meta:
